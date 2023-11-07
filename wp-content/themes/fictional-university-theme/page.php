239a866828b7a67ecbe61fb8adec0e9541e90114
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-11-07 00:34:22 -0300

    Added headr, footer, functions.php, ...

--- wp-content/themes/fictional-university-theme/page.php
+++ wp-content/themes/fictional-university-theme/page.php
@@ -3,9 +3,8 @@ get_header();
 
 while (have_posts()) {
   the_post(); ?>
-  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <h2>Page: <?php the_title(); ?></h2>
   <?php the_content(); ?>
-  <hr>
 <?php }
 
 get_footer();
